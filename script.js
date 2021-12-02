edc83c5c189201bc8e0fc86868a1e6ed2f7ba2bc
tod619 <trevorodwyer619@hotmail.com>
2021-12-02 16:37:10 +0000

    set minimum date to today on date picker in script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputContainer = document.getElementById('input-container')
+const countdownForm = document.getElementById('countdownForm')
+const dateEl = document.getElementById('date-picker')
+
+
+// Set Date Input Minimum With Todays Date
+const today = new Date().toISOString().split('T')[0]
+dateEl.setAttribute('min', today)
+

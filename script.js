7bc16562637ae7cdce8a84d70b9e2c01348a9d46
tod619 <trevorodwyer619@hotmail.com>
2021-12-02 16:55:46 +0000

    create function updateCountdown in script.js file

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const inputContainer = document.getElementById('input-container')
 const countdownForm = document.getElementById('countdownForm')
 const dateEl = document.getElementById('date-picker')
 
+let countdownTitle = ''
+let countdownDate = ''
+
 
 // Set Date Input Minimum With Todays Date
 const today = new Date().toISOString().split('T')[0]
 dateEl.setAttribute('min', today)
+
+// Populate the countdown with the info from the form
+function updateCountdown(e) {
+    e.preventDefault()
+    countdownTitle = e.srcElement[0].value
+    countdownDate = e.srcElement[1].value
+    
+}
+
+// Event Listners
+// Input form submit event
+countdownForm.addEventListener('submit', updateCountdown)
 
